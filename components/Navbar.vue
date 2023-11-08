c9ce408be1e56c1c82a3c6522b917840f13f4751
vazkya <142280637+vazkya@users.noreply.github.com>
2023-11-08 11:26:07 +0700

    Add files via upload

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <NuxtLink class="navbar-brand" to="/">Rekomendasi</NuxtLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" 
+        aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <NuxtLink class="nav-link active" aria-current="page" to="/pinjam">Pinjam</NuxtLink>
+            </li>
+            <li class="nav-item">
+              <NuxtLink class="nav-link" to="/history">History</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
